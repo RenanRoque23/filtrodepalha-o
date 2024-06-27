19cdef54afa9da16ea572b28c9fbef0a6b0602e7
RenanRoque23 <148238527+RenanRoque23@users.noreply.github.com>
2024-06-27 10:50:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,43 @@
-nozeX=0;
-nozeY=0;
+noseX=0;
+noseY=0;
 
-function preload(){
-    clown_nose = loadImage('https://i.postimg.cc/brWYfK1P/narix.png')
+// Primeiro definimos uma função preload() 
+function preload() {
+  clown_nose = loadImage('https://i.postimg.cc/brWYfK1P/narix.png');
 }
 
-function setup(){
-    canvas = createCanvas(300, 300)
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(300, 300);
-    video.hide();
-    
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+// Vamos criar uma função setup() e dentro dela criamos uma canvas (tela)
+function setup() {
+  canvas = createCanvas(300, 300);
+  // colocaremos canvas no centro da página usando
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
-    console.log('PoseNet Is Initalized');
+  console.log('PoseNet Is Initialized');
 }
 
 function gotPoses(results)
 {
-    if(results.length > 0)
-        {
-            console.log(results);
-            noseX = results[0].pose.nose.x-15;
-            noseY = results[0].pose.nose.y-15;
-        }
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-15;
+    noseY = results[0].pose.nose.y-15;
+  }
 }
-function draw(){
-    image(video, 0, 0, 300, 300);
-    image(clown_nose, noseX, noseY, 30, 30);
-}
-
-function take_snapshot(){
-    save('narix.png');
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 30, 30);
 }
 
-
-
-
-
-
+function take_snapshot(){   
+  
+  save('narix.png');
+}
